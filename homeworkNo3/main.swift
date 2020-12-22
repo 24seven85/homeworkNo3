572959d601ce51f3cad34f5637bb3bda89143dd5
maksimkjaulakis <24seven@live.ru>
2020-12-22 14:13:29 +0200

    add change method

--- homeworkNo3/main.swift
+++ homeworkNo3/main.swift
@@ -27,26 +27,53 @@ enum TrunkState {
 struct SportCar {
     let brand: String
     let model: String
-    let color: Color
     let wheels: Int
-    let yearOfManufacture: String
+    let yearOfManufacture: Int
     let trunkVolume: Int
     let engineState: EngineState
     let windowState: WindowState
     let trunkState: TrunkState
+    
+    var colors: Color {
+        didSet {
+            print("Машину перекрасили в \(colors)")
+        } willSet {
+            print("Изначально машина была \(colors)")
+        }
+    }
+    
+    mutating func changeColor(color: Color) {
+        self.colors = color
+    }
+    
+    
 }
-
-
-
 
 struct TruckCar {
     let brand: String
     let model: String
-    let color: Color
+    let colors: Color
     let wheels: Int
-    let yearOfManufacture: String
+    let yearOfManufacture: Int
     let trunkVolume: Int
     let engineState: EngineState
-    let windowState: WindowState
-    let trunkState: TrunkState
+    var windowState: WindowState
+    var trunkState: TrunkState {
+        didSet {
+            print("После стала \(trunkState)")
+        } willSet {
+            print("Изначально машина была \(trunkState)")
+        }
+    }
+    
+    mutating func changeWindow(window: WindowState) {
+        self.windowState = window
+    }
+    mutating func changeTrunk(trunk: TrunkState) {
+        self.trunkState = trunk
+    }
+    
 }
+
+
+
