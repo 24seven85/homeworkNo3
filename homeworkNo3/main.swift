2f031f9a326e032d80991ba820e6ccc48e9ad20f
maksimkjaulakis <24seven@live.ru>
2020-12-22 12:16:05 +0200

    add updates

--- homeworkNo3/main.swift
+++ homeworkNo3/main.swift
@@ -11,10 +11,29 @@ enum Color {
     case red, green, white, blue, silver
 }
 
+enum EngineState {
+    case start, stop
+}
+
+enum WindowState{
+    case open, close
+}
+
+enum TrunkState {
+    case full, half, empty
+}
+
+
 struct SportCar {
     let brand: String
     let model: String
     let color: Color
+    let wheels: Int
+    let yearOfManufacture: String
+    let trunkVolume: Int
+    let engineState: EngineState
+    let windowState: WindowState
+    let trunkState: TrunkState
 }
 
 
@@ -24,4 +43,10 @@ struct TruckCar {
     let brand: String
     let model: String
     let color: Color
+    let wheels: Int
+    let yearOfManufacture: String
+    let trunkVolume: Int
+    let engineState: EngineState
+    let windowState: WindowState
+    let trunkState: TrunkState
 }
